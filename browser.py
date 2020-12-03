93013939743af4a33c8bbe5019f6db00b6870bef
Mateus Jorge <52360561+suminz9@users.noreply.github.com>
2020-12-03 16:35:14 -0300

    commit-upload

--- browser.py
+++ browser.py
@@ -20,14 +20,14 @@ class InstagramLogin:
         login_button = browser.find_element_by_xpath("//button[@type='submit']")
         login_button.click()
         time.sleep(5)
-        InstaLikes(browser, hashtag)
+        InstaScript(browser, hashtag)
 
 class HomePage:
     def __init__(self, browser):
         self.browser = browser
         self.browser.get('https://www.instagram.com/')
 
-class InstaLikes:
+class InstaScript:
     def __init__(self, browser, hashtag):
         self.browser = browser
         browser.get('https://www.instagram.com/explore/tags/' + hashtag + '/')
@@ -48,23 +48,41 @@ class InstaLikes:
 
             except Exception: continue
 
+        like_flag = 5
+        like_count = 0
         likeable_image = len(image_hrefs)
         for image_href in image_hrefs:
             browser.get(image_href)
             time.sleep(3)
-            browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            like_count = like_count + 1
             try:
                 time.sleep(random.randint(2, 4))
-                like_button = lambda: browser.find_elements_by_xpath('//span[@aria-label="Like"]').click()
-                like_button().click()
+                if (like_count == like_flag):
+                    print("Sleeping ...")
+                    time.sleep(random.randint(18, 360))
+                    like_count = 0
+                else:
+                    like_button = lambda: browser.find_elements_by_css_selector('[aria-label="Like"]')[random.randint(0, 12)].click()
+                    like_button().click()
 
                 for second in reversed(range(0, random.randint(18, 28))):
-                    print("#" + hashtag + ': likeable images left: ' + str(likeable_image) + "  | Sleeping " + str(second))
-                    time.sleep(3)
+                    print("#" + hashtag + " | Sleeping " + str(second))
+                    time.sleep(1)
 
-            except Exception as e: time.sleep(3)
+            except Exception as e: time.sleep(2)
             likeable_image -= 1
 
+        follow_user = len(image_hrefs)
+        for image_href in image_hrefs:
+            browser.get(image_href)
+            time.sleep(3)
+
+            follow_button = lambda : browser.find_elements_by_css_selector("button")[random.randint(5, 10)].click()
+            follow_button().click()
+
+
 #Temporary
-insta_user = InstagramLogin(webdriver.Firefox(), "username", "password")
-insta_user.login("hashtag")
+insta_user = InstagramLogin(webdriver.Firefox(), "starfaker5301", "hello2world1999")
+hashtags = ['bike', 'bikes', 'cars']
+tag = random.choice(hashtags)
+insta_user.login(tag)
