3b53884d0fcd13576474967ef225ad6ed1dcd1e9
Mateus Jorge <52360561+suminz9@users.noreply.github.com>
2020-12-04 16:07:57 -0300

    commit

--- browser.py
+++ browser.py
@@ -6,41 +6,48 @@ from selenium.webdriver.common.keys import Keys
 import time
 import random
 
+# Global Variables
+
+Value = 3
+Buffer = 5
+Flag = 1
+
 class InstagramLogin:
     def __init__(self, browser, username, password):
         self.browser = browser
         self.username = username
         self.password = password
 
-    def Login(self, hashtag):
+    def Login(self, hashtag, flag):
         browser = self.browser
         HomePage(browser)
-        time.sleep(5)
+        time.sleep(Buffer)
         username_input = self.browser.find_element_by_css_selector("input[name='username']")
         password_input = self.browser.find_element_by_css_selector("input[name='password']")
         username_input.send_keys(self.username)
         password_input.send_keys(self.password)
         login_button = browser.find_element_by_xpath("//button[@type='submit']")
         login_button.click()
-        time.sleep(5)
-        InstaScript(browser, hashtag)
+        time.sleep(Buffer)
+        Script(browser, hashtag, flag)
 
 class HomePage:
     def __init__(self, browser):
         self.browser = browser
         self.browser.get('https://www.instagram.com/')
 
-class InstaScript:
-    def __init__(self, browser, hashtag):
+class Script:
+    def __init__(self, browser, hashtag, flag):
         self.browser = browser
         browser.get('https://www.instagram.com/explore/tags/' + hashtag + '/')
-        time.sleep(3)
+        time.sleep(Value)
+        flag = Flag
 
         image_hrefs = []
         for i in range(1, 7):
             try:
                 browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-                time.sleep(3)
+                time.sleep(Value)
                 href_in_range = browser.find_elements_by_tag_name('a')
                 href_in_range = [elem.get_attribute('href') for elem in href_in_range if '.com/p/' in elem.get_attribute('href')]
 
@@ -52,23 +59,22 @@ class InstaScript:
 
         likeable_image = len(image_hrefs)
         swap_flagRange = 0
+
         for image_href in image_hrefs:
             browser.get(image_href)
-            time.sleep(3)
-            random_countValue_generator = random.randint(1, 5)
-            random_flagValue_generator = random.randint(1, 5)
-            not_valueConst = random.randint(1, 2)
-            not_valueConst = not_valueConst/2
+            time.sleep(Value)
+
+            countValue, flagValue , notConst = randomGenerator()
             valueConst = 4
 
             try:
                 time.sleep(random.randint(2, 4))
 
-                if random_countValue_generator == random_flagValue_generator:
+                if countValue == flagValue:
                     print("Sleeping")
 
                     time.sleep(random.randint(18, 360))
-                    swap_flagRange = swap_flagRange + not_valueConst
+                    swap_flagRange = swap_flagRange + notConst
 
                     if swap_flagRange == valueConst:
                         print("Swap")
@@ -93,19 +99,25 @@ class InstaScript:
                             if random_pcountValue_generator == random_pflagValue_generator:
                                 print("Profile Sleeping:")
                                 time.sleep(random.randint(12, 25))
-                                getUser_profile = browser.find_elements_by_tag_name('a')
-                                getUser_profile.click()
-                                time.sleep(3)
-                                user_follow = browser.find_elements_by_xpath("//button[text()='Follow']")
-                                user_follow.click()
-                                time.sleep(3)
-                                getHome_feed = browser.find_elements_by_css_selector('[aria-label="Home"]')
-                                getHome_feed.click()
-                                time.sleep(3)
-                                search_hashtag = browser.find_elements_by_xpath("//span[text()='Search']")
-                                search_hashtag.sendkeys('#' + random.choice(hashtag))
-                                search_hashtag.sendkeys(Keys.ENTER)
-                                time.sleep(3)
+                                getUserProfile()
+                                followUser()
+                                getHome()
+                                getHashtag(browser, flag)
+
+
+                                # getUser_profile = browser.find_elements_by_tag_name('a')
+                                # getUser_profile.click()
+                                # time.sleep(3)
+                                # user_follow = browser.find_elements_by_xpath("//button[text()='Follow']")
+                                # user_follow.click()
+                                # time.sleep(3)
+                                # getHome_feed = browser.find_elements_by_css_selector('[aria-label="Home"]')
+                                # getHome_feed.click()
+                                # time.sleep(3)
+                                # search_hashtag = browser.find_elements_by_xpath("//span[text()='Search']")
+                                # search_hashtag.sendkeys('#' + random.choice(hashtag))
+                                # search_hashtag.sendkeys(Keys.ENTER)
+                                # time.sleep(3)
                             else:
                                 like_pimages = lambda: browser.find_elements_by_css_selector('[aria-label="Like"')[random.randint(0, 6)].click()
                                 like_pimages().click()
@@ -121,14 +133,52 @@ class InstaScript:
             except Exception as e: time.sleep(2)
             likeable_image -= 1
 
+def getUserProfile(self):
+    browser = self.browser
+    browser.find_elements_by_tag_name('a').click()
+    time.sleep(Value)
+
+def getHome(self):
+    browser = self.browser
+    browser.find_elements_by_css_selector("[aria-label='Home']").click()
+    time.sleep(Value)
+
+def getHashtag(browser, flag):
+    ObjHashtag = open("HASHTAGS.txt", "r")
+
+    if flag == Flag:
+        browser.find_element_by_xpath("//span[text()='Search']")
+        tag = browser.sendkeys("#" + random.choice(ObjHashtag.readlines()))
+        browser.sendkeys(Keys.ENTER)
+        time.sleep(Value)
+    else:
+        tag = random.choice(ObjHashtag.readlines())
+
+    return tag
+
+def followUser(self):
+    browser = self.browser
+    browser.find_element_by_xpath("//button[text()='Follow']").click()
+    time.sleep(Value)
+
+def randomGenerator():
+    parameterValue = random.randint(1, 5)
+    flagValue = random.randint(1, 5)
+    notConst = random.randint(1, 2)
+    notConst = notConst / 2
+
+    return parameterValue, flagValue, notConst
+
 def Main():
     ObjUser = open("USERNAME.txt", "r")
     ObjPassword = open("PASSWORD.txt", "r")
-    ObjHashtag = open("HASHTAGS.txt", "r")
 
-    insta_user = InstagramLogin(webdriver.Firefox(), ObjUser.read(), ObjPassword.read())
-    tag = random.choice(ObjHashtag.readlines())
-    insta_user.Login(tag)
+    flag = 0
+    browser = webdriver.Firefox()
+
+    insta_user = InstagramLogin(browser, ObjUser.read(), ObjPassword.read())
+    tag = getHashtag(browser, flag)
+    insta_user.Login(tag, flag)
 
 if __name__ == '__main__':
     Main()
